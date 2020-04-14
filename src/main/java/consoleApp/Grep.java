ff12d6524be87a8f110098569f210a7b2ac9b63e
Ekaterina Gerasina <gerasinaes@mail.ru>
2020-04-14 12:04:45 +0400

    .

--- src/main/java/consoleApp/Grep.java
+++ src/main/java/consoleApp/Grep.java
@@ -14,38 +14,38 @@ public class Grep {
         this.word = word;
         this.inputName = inputName;
     }
-public void logics(int filtrationCondition,int ignoreWordRegister, Pattern regex)  {
+public void logics(boolean filtrationCondition,boolean ignoreWordRegister, Pattern regex)  {
        String s;
        try(BufferedReader br = new BufferedReader(new FileReader(inputName)))
        {
-           if(regex ==null && ignoreWordRegister ==0 && filtrationCondition==0) { //nothing found
+           if(regex ==null && ignoreWordRegister =false && !filtrationCondition) { //nothing found
                while((s=br.readLine()) !=null){
                    s = br.readLine();
                     System.out.println(s);
                }
            }
-           if(regex !=null && ignoreWordRegister ==0 && filtrationCondition==0) { //only regex found
+           if(regex !=null && ignoreWordRegister =false && filtrationCondition=false) { //only regex found
                    while((s=br.readLine()) !=null){
                        s = br.readLine();
                        Matcher matcher = regex.matcher(s);
                        if (matcher.find()) System.out.println(s);
                    }
            }
-           if(regex !=null && ignoreWordRegister ==1 && filtrationCondition==0) { //regex and ignoreWordRegister found
+           if(regex !=null && ignoreWordRegister =true && filtrationCondition=false) { //regex and ignoreWordRegister found
                while((s=br.readLine()) !=null){
                    s = br.readLine().toLowerCase();
                    Matcher matcher = regex.matcher(s);
                    if (matcher.find()) System.out.println(s);
                }
            }
-           if(regex !=null && ignoreWordRegister ==0 && filtrationCondition==1) { //regex and filtrationCondition found
+           if(regex !=null && ignoreWordRegister =false && filtrationCondition=true) { //regex and filtrationCondition found
                while((s=br.readLine()) !=null){
                    s = br.readLine();
                    Matcher matcher = regex.matcher(s);
                    if (matcher.find()==false) System.out.println(s);
                }
            }
-           if(regex !=null && ignoreWordRegister ==1 && filtrationCondition==1) { //all found
+           if(regex !=null && ignoreWordRegister =true && filtrationCondition==1) { //all found
                while((s=br.readLine()) !=null){
                    s = br.readLine().toLowerCase();
                    Matcher matcher = regex.matcher(s);
